ff3de7ef9dbb1e2e787b60e6dc98d5516cd5cb11
AndreasVan <anders@honeybees.se>
2017-04-15 22:38:02 +0200

    Update dropcam.py
    Picamera capturing to a different folder

--- dropcam.py
+++ dropcam.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 from subprocess import call
@@ -7,14 +6,27 @@ import sys
 import time
 import picamera
 import datetime
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO_RED_LIGHT = 22
+GPIO.output(GPIO_RED_LIGHT, True)
+time.sleep(1)
+GPIO.output(GPIO_RED_LIGHT, False)
+time.sleep(1)
+GPIO.output(GPIO_RED_LIGHT, True)
+time.sleep(1)
+GPIO.output(GPIO_RED_LIGHT, False)
+
 timestamp=datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 camera = picamera.PiCamera()
 while  True:
          camera.resolution=(1024,768)
          camera.rotation=0
          camera.start_preview()
-         camera.capture(timestamp+".jpg")
-
+         camera.capture("/home/pi/usbdrv/" + timestamp+".jpg")
          photofile = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/usbdrv/"+timestamp+".jpg "+timestamp+".jpg "
          call([photofile], shell=True)
          print "wait 15 minutes"
